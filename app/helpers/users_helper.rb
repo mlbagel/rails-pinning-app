f8d58ada4ea18cd88fcee76c800b92a156cedfc0
Michelle <mlbagell@gmail.com>
2016-09-22 11:17:03 -0400

    moved Shoulda-matchers to spec file and removed from users_helper file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
-#module UsersHelper
-#  Shoulda::Matchers.configure do |config|
-#    config.integrate do |with|
-#      with.test_framework :rspec
-#      with.library :rails
-#    end
-#  end
-
-#end
+module UsersHelper
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,16 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+Shoulda::Matchers.configure do |config|
+   config.integrate do |with|
+    with.test_framework :rspec
+     with.library :rails
+   end
+ end
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
-  
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
